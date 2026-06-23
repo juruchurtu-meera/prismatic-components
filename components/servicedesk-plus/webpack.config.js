458d67fc5bd0821573b5bd983b477a3c392ebf88
Prismatic <noreply@prismatic.io>
2026-06-23 22:11:32 +0000

    Updates to the following components: servicedesk-plus. (f9d7a84)

--- components/servicedesk-plus/webpack.config.js
+++ components/servicedesk-plus/webpack.config.js
@@ -1,35 +1,35 @@
 const path = require("node:path");
 const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-    mode: "production",
-    target: "node",
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: "assets", to: path.resolve(__dirname, "dist") }],
-        }),
+  mode: "production",
+  target: "node",
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "assets", to: path.resolve(__dirname, "dist") }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        sideEffects: false,
+      },
+      {
+        test: /\.ts/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
-    module: {
-        rules: [
-            {
-                sideEffects: false,
-            },
-            {
-                test: /\.ts/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    optimization: {
-        usedExports: true,
-    },
-    entry: "./src/index.ts",
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "dist"),
-        libraryTarget: "commonjs2",
-    },
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  optimization: {
+    usedExports: true,
+  },
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "commonjs2",
+  },
 };
